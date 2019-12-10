9eb7e70d8d815b8dc92617fbd713c4abb22fe400
lynn.tran18@gmail.com <lyntran@lyntran-dev.redmond.corp.microsoft.com>
2019-12-09 16:21:28 -0800

    Temporarily commit stuff

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -8,9 +8,31 @@ fn main() {
     let mut input = String::new();
     let _ = reader.read_line(&mut input);
 
-    let mut output : Vec<u32> = input.trim().split(',').map(|x| x.parse::<u32>().unwrap()).collect();
+    let mut output : Vec<usize> = input.trim().split(',').map(|x| x.parse::<usize>().unwrap()).collect();
+    println!("Input : {:?}", output);
+    let len = output.len();
 
-    //for item in &output {
+/*
+    for (i, element) in output.iter_mut().enumerate() {
+        println!("{:?}", element);
+        match element {
+            99 => { println!("Output part 1: {:?}", output[0]); break; },
+            1 => {
+                    if i+3 < len {
+                        let sum = output[i+1] + output[i+2];
+                        output[i+3] = sum;
+                    } else {
+                        println!("Out of range");
+                    }
+                    //let sum = output[item.next()] + output[item.next()];
+                    //outpupt[item.next()] = sum;
+                 },
+            2 => {},
+            _ => println!("Opcode {:?}", element)//panic!("Incorrect opcode: {:?}", element)
+        }
+    }
+*/
+/*
     for item in output.iter_mut() {
         println!("{:?}", item);
         match item {
@@ -23,5 +45,28 @@ fn main() {
             _ => panic!("Incorrect opcode: {:?}", item)
         }
 
+    }
+*/
+
+    let mut iter = output.iter_mut();
+
+    while let Some(element) = iter.next() {
+        println!("{:?}", element);
+        match element {
+            99 => { println!("Output part 1: {:?}", output[0]); break; },
+            1 => {
+                    if let Some(pos1) = iter.next() {
+                        println!("Value pos1: {:?}", pos1);
+                        if let Some(pos2) = iter.next() {
+                            println!("Value pos2: {:?}", pos2);
+                            if let Some(pos3) = iter.next() {
+                                println!("{}", output);
+                            }
+                        }
+                    }
+                 },
+            2 => {},
+            _ => println!("Value: {:?}", element)
+        }
     }
 }
